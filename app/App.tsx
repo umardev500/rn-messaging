96de94b637c10da11484c86ef15cd0591107710d
Umar <umar.dev500@gmail.com>
2024-03-13 11:38:33 +0700

    add initial messaging screen

--- app/App.tsx
+++ app/App.tsx
@@ -1,8 +1,19 @@
 import '@/assets/styles/app.css';
+import { MessagingScreen } from '@/screens';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
 
+const Stack = createNativeStackNavigator();
+
 const App = (): React.JSX.Element => {
-  return <></>;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="MessagingScreen" component={MessagingScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;
